41dc5838dbeff77df2895e902a637406b49c844c
Edward Nickson <edward.nickson@hotmail.com>
2020-12-01 16:23:31 +1100

    Add day1 solution and day2 skeleton

--- day1.py
+++ day1.py
@@ -2,3 +2,20 @@ from fileUtils import loadInputLines
 
 lines = loadInputLines(2020, 1)
 
+def part1():
+    intLines = list(map(int, lines))
+    for i in range(0, len(intLines)-1):
+        for j in range(i+1, len(intLines)):
+            if intLines[i] + intLines[j] == 2020:
+                return intLines[i] * intLines[j]
+
+def part2():
+    intLines = list(map(int, lines))
+    for i in range(0, len(intLines)-2):
+        for j in range(i+1, len(intLines)-1):
+            for k in range(j+1, len(intLines)):
+                if intLines[i] + intLines[j] + intLines[k]== 2020:
+                    return intLines[i] * intLines[j] * intLines[k]
+
+print(part1())
+print(part2())
---
+++ day2.py
@@ -0,0 +1,12 @@
+from fileUtils import loadInputLines
+
+lines = loadInputLines(2020, 2)
+
+def part1():
+    return
+
+def part2():
+    return
+
+print(part1())
+print(part2())
