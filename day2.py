39e8e4dcab900f0990eae65a861829c861b64fb3
Edward <edward.nickson@apparel21.com>
2020-12-02 16:37:20 +1100

    Add not so great day 2 solution and day 3 skeleton

--- day2.py
+++ day2.py
@@ -1,12 +1,33 @@
 from fileUtils import loadInputLines
 
-lines = loadInputLines(2020, 2)
+#TODO - Learn Regex
 
+lines = loadInputLines(2020, 2)
 def part1():
-    return
+    c = 0
+    for line in lines:
+        a = line.split(':')
+        l = a[0].split(' ')[1]
+        num = a[0].split(' ')[0].split('-')
+        count = a[1].count(l)
+        if count >= int(num[0]) and count <= int(num[1]):
+            c += 1
+    return c
 
 def part2():
-    return
+    c = 0
+    for line in lines:
+        a = line.split(':')
+        l = a[0].split(' ')[1]
+        num = a[0].split(' ')[0].split('-')
+        t = 0
+        if (a[1][int(num[0])] == l):
+            t += 1
+        if (a[1][int(num[1])] == l):
+            t += 1
+        if t == 1:
+            c += 1
+    return c
 
 print(part1())
 print(part2())
---
+++ day3.py
@@ -0,0 +1,11 @@
+from fileUtils import loadInputLines
+
+lines = loadInputLines(2020, 3)
+def part1():
+    return
+
+def part2():
+    return
+
+print(part1())
+print(part2())
