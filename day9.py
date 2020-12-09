b37c452bfd5c0aeb80d1ccefe1bdc90bb80ac003
Edward <edward.nickson@apparel21.com>
2020-12-09 17:02:59 +1100

    add shitty day9 solution

--- day9.py
+++ day9.py
@@ -1,34 +1,36 @@
 from aocUtils import loadInputLines, printT
 import re
 
-lines = loadInputLines(2020, 8)
-origIns = [line.split(' ') for line in lines]
-
-def runBoot(ins):
-    acc = 0
-    i = 0
-    opsRun = set()
-    while True:
-        if i in opsRun: #infinite Loop
-            return (0, acc)
-        if i == len(ins): #terminated correctly
-            return (1, acc)
-        op = ins[i][0]
-        args = ins[i][1:]
-        opsRun.add(i)
-        if op == 'nop':
-            i += 1
-        elif op == 'acc':
-            acc += int(args[0])
-            i += 1
-        elif op == 'jmp':
-            i += int(args[0])
+lines = loadInputLines(2020, 9)
+nums = [int(line) for line in lines]
 
 def part1():
-    return
+    for i in range(25, len(nums)-25):
+        valid = False
+        for x in range(i-25, i-1):
+            for y in range(x+1, i):
+                if nums[x] + nums[y] == nums[i]:
+                    valid =True
+        if valid == False:
+            return nums[i]
 
 def part2():
-    return
+    for i in range(0, len(nums)-1):
+        sum = 0
+        m1 = None
+        m2 = None
+        for j in range(i, len(nums)):
+            sum += nums[j]
+            if m1 == None:
+                m1 = nums[j]
+            else:
+                m1 = min(nums[j], m1)
+            if m2 == None:
+                m2 = nums[j]
+            else:
+                m2 = max(nums[j], m2)
+            if sum == 1930745883:
+                return m1 + m2
 
 printT(part1())
 printT(part2())
