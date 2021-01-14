b50d1f9ac531cdc321bf0964fec87ca2ba546b88
Edward <edward.nickson@apparel21.com>
2021-01-15 08:11:08 +1100

    some more solutions

---
+++ 2019/Day10.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace _2019
+{
+    public class Day10 : Solver
+    {
+        readonly List<(int, int)> asteroids = new List<(int, int)>();
+        (int, int) selectedAsteroid;
+
+        public Day10(string input) : base(input)
+        {
+            var lines = input.Split("\n");
+            var y = 0;
+            foreach (var line in lines)
+            {
+                for (int x = 0; x < line.Length; x++)
+                {
+                    if (line[x] == '#')
+                        asteroids.Add((x, y));
+                }
+                y++;
+            }
+        }
+
+        public class RelativeAsteroid
+        {
+            public (int, int) coord;
+            public decimal degrees;
+            public decimal radius;
+            const int PRECISION = 4;
+
+            public RelativeAsteroid((int, int) coord, (int, int) origin)
+            {
+                this.coord = coord;
+                var vector = (coord.Item1 - origin.Item1, coord.Item2 - origin.Item2);
+                radius = decimal.Round((decimal)Math.Sqrt(Math.Pow(vector.Item1, 2) + Math.Pow(vector.Item2, 2)), PRECISION);
+                degrees = (decimal)(Math.Atan2(vector.Item2, vector.Item1) - Math.Atan2(-1, 0));
+                if (vector.Item1 < 0 && vector.Item2 < 0)
+                    degrees = (decimal)Math.PI * 2 + degrees;
+                degrees = decimal.Round(degrees, PRECISION);
+            }
+        }
+
+        public override object Part1()
+        {
+            var max = 0;
+            foreach (var asteroid in asteroids)
+            {
+                var visibleAsteroids = asteroids.Where(x => x != asteroid)
+                                .Select(x => new RelativeAsteroid(x, asteroid))
+                                .Select(x => x.degrees).ToHashSet().Count;
+                if (visibleAsteroids > max)
+                {
+                    selectedAsteroid = asteroid;
+                    max = visibleAsteroids;
+                }
+            }
+            return max;
+        }
+
+        public override object Part2()
+        {
+            var targets = asteroids.Where(x => x != selectedAsteroid)
+                            .Select(x => new RelativeAsteroid(x, selectedAsteroid))
+                            .OrderBy(x => x.degrees).ThenBy(x=>x.radius).ToList();
+            decimal d = -1;
+            int i = 0;
+            int destroyed = 0;
+            while (true)
+            {
+                if (i >= targets.Count)
+                    i = 0;
+                if (targets[i].degrees > d)
+                {
+                    destroyed++;
+                    if (destroyed == 200)
+                        return targets[i].coord.Item1 * 100 + targets[i].coord.Item2;
+                    d = targets[i].degrees;
+                    targets.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
--- 2019/Day11.cs
+++ 2019/Day11.cs
@@ -7,10 +7,10 @@ using System.Text;
 
 namespace _2019
 {
-    public class Day9 : Solver
+    public class Day11 : Solver
     {
         long[] intCodes;
-        public Day9(string input) : base(input)
+        public Day11(string input) : base(input)
         {
             intCodes = input.Split(",").Select(x => long.Parse(x)).ToArray();
         }
@@ -169,25 +169,98 @@ namespace _2019
             }
         }
 
+        public class Robot
+        {
+            (int x, int y) direction = (0, -1);
+            (int x, int y) location = (0, 0);
+            public readonly Dictionary<(int x, int y), long> tiles = new Dictionary<(int x, int y), long>();
+            readonly Computer computer;
+
+            public Robot(long[] program, int startingTileColour)
+            {
+                tiles[location] = startingTileColour;
+                computer = new Computer(program);
+            }
+
+            public void PaintTiles()
+            {
+                computer.Run();
+                while (!computer.state.halted)
+                {
+                    computer.Run(GetCurrentTile);
+                    PaintCurrentTile(computer.state.output.Value);
+                    computer.Run();
+                    TurnAndMove(computer.state.output.Value);
+                    computer.Run();
+                }
+            }
+
+            public int TilesPainted => tiles.Count;
+
+            void TurnAndMove(long programOutput)
+            {
+                direction = programOutput switch
+                {
+                    1 => (direction.y * -1, direction.x),
+                    0 => (direction.y, direction.x * -1),
+                    _ => throw new Exception($"Invalid rotation input {programOutput}")
+                };
+                location = (location.x + direction.x, location.y + direction.y);
+            }
+
+            long GetCurrentTile => tiles.GetValueOrDefault(location, 0);
+
+            void PaintCurrentTile(long programOuput)
+            {
+                if (programOuput != 0 && programOuput != 1)
+                    throw new Exception($"Invalid paint input {programOuput}");
+                tiles[location] = programOuput;
+            }
+        }
+
         public override object Part1()
         {
-            var computer = new Computer(intCodes);
-            computer.Run();
-            computer.Run(1);
-            while (!computer.state.halted)
-            {
-                Console.WriteLine(computer.state.output);
-                computer.Run();
-            }
-            return computer.state.output;
+            var robot = new Robot(intCodes, 0);
+            robot.PaintTiles();
+            return robot.TilesPainted;
         }
 
         public override object Part2()
         {
-            var computer = new Computer(intCodes);
-            computer.Run();
-            computer.Run(2);
-            return computer.state.output;
+            var robot = new Robot(intCodes, 1);
+            robot.PaintTiles();
+            (int x, int y) min = (0,0), max = (0,0);
+            foreach (var (key, value) in robot.tiles)
+            {
+                if (value == 1)
+                {
+                    min = (Math.Min(min.x, key.x), Math.Min(min.y, key.y));
+                    max = (Math.Max(max.x, key.x), Math.Max(max.y, key.y));
+                }
+            }
+            long[,] tiles = new long[max.x - min.x + 1, max.y - min.y + 1];
+            foreach (var (key, value) in robot.tiles)
+            {
+                if (value == 1)
+                    tiles[key.x - min.x, key.y - min.y] = value;
+            }
+            PrintArray(tiles, 2, (1, '8'));
+            return null;
+        }
+
+        void PrintArray(long[,] array, int zoom, (int, char) replace)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int x = 0; x < zoom; x++)
+                {
+                    for (int i = 0; i < array.GetLength(0); i++)
+                    {
+                        Console.Write(string.Concat(Enumerable.Repeat(array[i, j] == replace.Item1 ? replace.Item2.ToString() : " ", zoom)));
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
